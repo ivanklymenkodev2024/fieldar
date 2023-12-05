22cd91c39bc24b8885552973bce500c8dfa7e33f
ivanklymenkodev <ivanklymenko.dev@outlook.com>
2023-12-04 19:56:14 -0500

    [update]: activity except combobox

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -3,17 +3,22 @@
 import Header from "@/components/header";
 import SideBar from "@/components/sidebar";
 
+import Image from "next/image";
+
+import commentIcon from "../../public/icons/Comment.png";
+import editIcon from "../../public/icons/EditIcon.png";
+
 const ActivityPage = () => {
   return (
     <div className="flex">
-      <SideBar index={3}/>
+      <SideBar index={3} />
       <div className="absolute left-[320px] w-panel h-[100vh] bg-gray-4">
         <Header title={"Company Activity"} />
         <div className="px-[32px] pb-[14px] flex flex-col">
           <div className="max-w-[1024px] flex justify-end items-end">
             <div className="flex flex-row items-center">
               <p className="ml-10 font-small font-light text-gray-10">
-                Filter by: 
+                Filter by:
               </p>
               <select className="bg-gray-3 border-gray-3 border-r-[30px] text-gray-11 placeholder:italic rounded-[25px] font-small px-[23px] py-[14px] w-[260px] m-2 mr-5 outline-none ">
                 <option>South Hampton L...</option>
@@ -26,57 +31,129 @@ const ActivityPage = () => {
           <div className="max-w-[1024px] flex flex-col bg-gray-3 h-actable rounded-[24px]">
             <div className="">
               <div className="flex bg-gray-2 px-[30px] py-[15px] rounded-t-[24px]">
-                <p className="text-gray-10 text-small font-semibold mr-2">Project:</p>
-                <p className="text-white text-small font-semibold">South Hampton Library</p>
+                <p className="text-gray-10 text-small font-semibold mr-2">
+                  Project:
+                </p>
+                <p className="text-white text-small font-semibold">
+                  South Hampton Library
+                </p>
               </div>
               <div className="flex bg-gray-3 px-[30px] py-[15px] justify-between items-center border-gray-4 border-b-[1px]">
                 <div className="flex justify-start items-center">
-                  <div className="w-[30px] h-[30px] bg-white mr-[17px]"></div>
-                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">Kyle Szostek</p>
-                  <p className="text-gray-10 text-primary font-semibold mr-1">edited model:</p>
-                  <p className="text-red-primary text-primary font-semibold">Corridor D9 </p>
+                  <div className="w-[30px] h-[30px] mr-[17px] flex justify-center items-center">
+                    <Image
+                      src={editIcon}
+                      width={22}
+                      height={20}
+                      alt="commnet"
+                      className="opacity-60"
+                    />
+                  </div>
+                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">
+                    Kyle Szostek
+                  </p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    edited model:
+                  </p>
+                  <p className="text-red-primary text-primary font-semibold">
+                    Corridor D9{" "}
+                  </p>
                 </div>
                 <div>
-                <p className="text-gray-10 text-primary font-semibold mr-1">9/15/2023 9:34am</p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    9/15/2023 9:34am
+                  </p>
                 </div>
               </div>
               <div className="flex bg-gray-3 px-[30px] py-[15px] justify-between items-center border-gray-4 border-b-[1px]">
                 <div className="flex justify-start">
-                  <div className="w-[30px] h-[30px] bg-white mr-[17px]"></div>
-                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">John Jones</p>
-                  <p className="text-gray-10 text-primary font-semibold mr-1">created a markup in model:</p>
-                  <p className="text-red-primary text-primary font-semibold">Room 123 </p>
+                  <div className="w-[30px] h-[30px] mr-[17px] flex justify-center items-center">
+                    <Image
+                      src={commentIcon}
+                      width={22}
+                      height={20}
+                      alt="commnet"
+                      className="opacity-60"
+                    />
+                  </div>
+                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">
+                    John Jones
+                  </p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    created a markup in model:
+                  </p>
+                  <p className="text-red-primary text-primary font-semibold">
+                    Room 123{" "}
+                  </p>
                 </div>
                 <div>
-                <p className="text-gray-10 text-primary font-semibold mr-1">9/13/2023 1:15pm</p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    9/13/2023 1:15pm
+                  </p>
                 </div>
               </div>
             </div>
             <div className="">
               <div className="flex bg-gray-2 p-[30px] py-[15px]">
-                <p className="text-gray-10 text-small font-semibold mr-2">Project:</p>
-                <p className="text-white text-small font-semibold">NASA Headquarters</p>
+                <p className="text-gray-10 text-small font-semibold mr-2">
+                  Project:
+                </p>
+                <p className="text-white text-small font-semibold">
+                  NASA Headquarters
+                </p>
               </div>
               <div className="flex bg-gray-3 px-[30px] py-[15px] justify-between items-center border-gray-4 border-b-[1px]">
                 <div className="flex justify-start items-center">
-                  <div className="w-[30px] h-[30px] bg-white mr-[17px]"></div>
-                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">Kyle Szostek</p>
-                  <p className="text-gray-10 text-primary font-semibold mr-1">created model:</p>
-                  <p className="text-red-primary text-primary font-semibold">Launch Pad D17</p>
+                  <div className="w-[30px] h-[30px] mr-[17px] flex justify-center items-center">
+                    <Image
+                      src={editIcon}
+                      width={22}
+                      height={20}
+                      alt="commnet"
+                      className="opacity-60"
+                    />
+                  </div>
+                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">
+                    Kyle Szostek
+                  </p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    created model:
+                  </p>
+                  <p className="text-red-primary text-primary font-semibold">
+                    Launch Pad D17
+                  </p>
                 </div>
                 <div>
-                <p className="text-gray-10 text-primary font-semibold mr-1">9/15/2023 9:34am</p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    9/15/2023 9:34am
+                  </p>
                 </div>
               </div>
               <div className="flex bg-gray-3 px-[30px] py-[15px] justify-between items-center border-gray-4 border-b-[1px]">
                 <div className="flex justify-start">
-                  <div className="w-[30px] h-[30px] bg-white mr-[17px]"></div>
-                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">John Jones</p>
-                  <p className="text-gray-10 text-primary font-semibold mr-1">responded to a markup in:</p>
-                  <p className="text-red-primary text-primary font-semibold">Control Center 266</p>
+                  <div className="w-[30px] h-[30px] mr-[17px] flex justify-center items-center">
+                    <Image
+                      src={commentIcon}
+                      width={22}
+                      height={20}
+                      alt="commnet"
+                      className="opacity-60"
+                    />
+                  </div>
+                  <p className="text-gray-10 text-primary font-semibold mr-[40px]">
+                    John Jones
+                  </p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    responded to a markup in:
+                  </p>
+                  <p className="text-red-primary text-primary font-semibold">
+                    Control Center 266
+                  </p>
                 </div>
                 <div>
-                <p className="text-gray-10 text-primary font-semibold mr-1">9/13/2023 1:15pm</p>
+                  <p className="text-gray-10 text-primary font-semibold mr-1">
+                    9/13/2023 1:15pm
+                  </p>
                 </div>
               </div>
             </div>
