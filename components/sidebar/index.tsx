e3c31aa16903499cceacda8cd18e5302e8380fd8
ivanklymenkodev <ivanklymenko.dev@outlook.com>
2023-12-09 02:50:45 -0500

    [update]: sidebar profile image

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -11,9 +11,47 @@ import activityIcon from "../../public/icons/Comment.png";
 import subscriptionIcon from "../../public/icons/SubscriptionIcon.png";
 import settingsIcon from "../../public/icons/SettingsIcon.png";
 import logoutIcon from "../../public/icons/LogoutIcon.png";
+
+import defaultUser from "../../public/icons/User.png";
+
+import firebase_app from "../../config";
+import { getAuth } from "firebase/auth";
+import { getFirestore, onSnapshot } from "firebase/firestore";
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+const auth = getAuth();
+const functions = getFunctions();
+
+const database = getDatabase(firebase_app);
+
 import Link from "next/link";
+import { child, get, getDatabase, ref } from "firebase/database";
+import { useState } from "react";
 
 const SideBar: React.FC<SidebarProps> = ({ index }: SidebarProps) => {
+  const [picUrl, setPicUrl] = useState("");
+
+  auth.onAuthStateChanged(function (user: any) {
+    if (user != null) {
+      const uid = user.uid;
+
+      const dbRef = ref(getDatabase());
+      get(child(dbRef, `users/${uid}`))
+        .then((snapshot: any) => {
+          if (snapshot.exists()) {
+            setPicUrl(snapshot.val().PhotoURL);
+          } else {
+            console.log("No data available");
+          }
+        })
+        .catch((error: any) => {
+          console.error(error);
+        });
+    } else {
+      console.log(null);
+    }
+  });
+
   return (
     <div className="fixed left-0 top-0 h-[100vh] w-sidebar bg-gray-2">
       <div className=" w-full flex flex-col justify-center items-center my-[30px]">
@@ -21,11 +59,11 @@ const SideBar: React.FC<SidebarProps> = ({ index }: SidebarProps) => {
         <Link href="/profile">
           <div className="border-gray-4 border-[8px] rounded-[50%]">
             <Image
-              src={profilePic}
+              src={picUrl == "" ? defaultUser : picUrl}
               width={100}
               height={100}
               alt="Profile Image"
-              className="rounded-[50%] border-[4px] border-gray-3 shadow-md drop-shadow-0 drop-shadow-y-3 blur-6"
+              className="rounded-[50%] border-[4px] border-gray-3 shadow-md drop-shadow-0 drop-shadow-y-3 blur-6 w-[100px] h-[100px]"
             />
           </div>
         </Link>
