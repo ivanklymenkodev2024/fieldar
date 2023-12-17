efe2350acbf025f49e8efd13089fa00402a8cd1e
ivanklymenkodev <ivanklymenko.dev@outlook.com>
2023-12-17 18:45:30 -0500

    [add]: update billing details function

--- app/edit-subscription/page.tsx
+++ app/edit-subscription/page.tsx
@@ -29,6 +29,22 @@ const auth = getAuth();
 const database = getDatabase(firebase_app);
 
 import { getFunctions, httpsCallable } from "firebase/functions";
+
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  AddressElement,
+  CardCvcElement,
+  CardElement,
+  CardExpiryElement,
+  CardNumberElement,
+  Elements,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
 import Link from "next/link";
 
 const functions = getFunctions();
@@ -37,8 +53,36 @@ const cRemoveProjectsFromEnterprise = httpsCallable(
   "removeProjectsFromEnterprise"
 );
 
+const cUpdateBillingDetails = httpsCallable(functions, "updateBillingDetails");
+
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      color: "#303238",
+      fontSize: "16px",
+      fontFamily: '"Open Sans", sans-serif',
+      bacgroundColor: "#fff",
+      letterSpacing: "0.025em",
+      "::placeholder": {
+        color: "#CFD7DF",
+      },
+      invalid: {
+        color: "#e5424d",
+        ":focus": {
+          color: "#303238",
+        },
+      },
+    },
+  },
+};
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
 const EditSubscriptionPage = () => {
   const [isSide, setIsSide] = useState(false);
+
+  const stripe = useStripe();
+  const elements = useElements();
 
   const {
     user,
@@ -52,6 +96,65 @@ const EditSubscriptionPage = () => {
     updateContext,
   } = useGlobalContext();
 
+  const [isShowUpdateBillingDetails, setIsShowUpdateBillingDetails] =
+    useState(false);
+
+  const updateBillingDetails = () => {
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const cardNumberElement = elements.getElement(CardNumberElement);
+    const addressElement = elements.getElement(AddressElement);
+    addressElement?.getValue().then((res: any) => {
+      if (cardNumberElement) {
+        setIsLoading(true);
+        stripe
+          .createPaymentMethod({
+            type: "card",
+            card: cardNumberElement,
+            billing_details: {
+              name: res.value.name,
+              address: res.value.address,
+            },
+          })
+          .then((result: any) => {
+            console.log(result);
+            if (result.error) {
+              setIsLoading(false);
+              // toast.warning(result.error.message);
+              return;
+            }
+            cUpdateBillingDetails({
+              paymentMethodId: result.paymentMethod.id,
+              billingDetails: {
+                name: res.value.name || "",
+                addressLine1: res.value.address.line1 || "",
+                addressLine2: res.value.address.line2 || "",
+                addressCity: res.value.address.city || "",
+                addressState: res.value.address.state || "",
+                addressCountry: res.value.address.country || "",
+                addressZip: res.value.address.postal_code || "",
+              },
+            })
+              .then((res: any) => {
+                toast.success(res.data.message);
+                setIsLoading(false);
+                setIsShowUpdateBillingDetails(false);
+                updateContext();
+              })
+              .catch((error) => {
+                setIsLoading(false);
+              });
+          })
+          .catch((error) => {
+            toast.warning(error.message);
+            setIsLoading(false);
+          });
+      }
+    });
+  };
+
   const [companyProjectCount, setCompanyProjectCount] = useState(
     company.SubscriptionDetails != undefined
       ? company.SubscriptionDetails.projectQuantity
@@ -60,8 +163,7 @@ const EditSubscriptionPage = () => {
 
   const [selectedProjects, setSelectedProjects] = useState([]);
 
-  const [isShowBillingDetailsModal, setIsShowBillingDetailsModal] =
-    useState(false);
+  const [isShowInvoiceModal, setIshowInvoiceModal] = useState(false);
 
   const handleRemoveProjects = () => {
     setIsLoading(true);
@@ -310,7 +412,7 @@ const EditSubscriptionPage = () => {
                           <button
                             className="text-center mb-[20px] bg-gray-7-5 rounded-[27px] px-[50px] py-[10px] text-white"
                             onClick={() => {
-                              setIsShowBillingDetailsModal(true);
+                              setIshowInvoiceModal(true);
                             }}
                           >
                             View Invoices
@@ -318,7 +420,7 @@ const EditSubscriptionPage = () => {
                           <button
                             className="text-center mb-[20px] bg-gray-7-5 rounded-[27px] px-[50px] py-[10px] text-white"
                             onClick={() => {
-                              setIsShowBillingDetailsModal(true);
+                              setIsShowUpdateBillingDetails(true);
                             }}
                           >
                             Edit Billing Details
@@ -570,7 +672,7 @@ const EditSubscriptionPage = () => {
           </div>
         </div>
       )}
-      {isShowBillingDetailsModal && (
+      {isShowInvoiceModal && (
         <div
           id="modal_single"
           className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
@@ -578,7 +680,7 @@ const EditSubscriptionPage = () => {
           <div className="relative p-4 w-full max-w-[620px] max-h-full">
             <div
               className="fixed bg-black opacity-30 w-[100vw] h-[100vh] left-0 top-0"
-              onClick={() => setIsShowBillingDetailsModal(false)}
+              onClick={() => setIshowInvoiceModal(false)}
             ></div>
             <div className="relative bg-gray-4 border-[1px] border-gray-6 rounded-[26px] shadow-md drop-shadow-0 drop-shadow-y-3 blur-6">
               <div className="flex items-center justify-center p-4 md:p-5 ">
@@ -588,7 +690,7 @@ const EditSubscriptionPage = () => {
                 <button
                   type="button"
                   className="absolute right-0 mr-[20px] text-white bg-gray-8 hover:bg-gray-200 hover:text-gray-900 rounded-[55px] shadow-md drop-shadow-0 drop-shadow-y-3 blur-6 text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                  onClick={() => setIsShowBillingDetailsModal(false)}
+                  onClick={() => setIshowInvoiceModal(false)}
                 >
                   <Image src={closeIcon} width={20} height={20} alt="close" />
                   <span className="sr-only">Close modal</span>
@@ -632,9 +734,116 @@ const EditSubscriptionPage = () => {
           </div>
         </div>
       )}
+      {isShowUpdateBillingDetails && (
+        <div
+          id="modal_single"
+          className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        >
+          <div className="relative p-4 w-full max-w-[520px] max-h-full">
+            <div
+              className="fixed bg-black opacity-30 w-[100vw] h-[100vh] left-0 top-0"
+              onClick={() => setIsShowUpdateBillingDetails(false)}
+            ></div>
+            <div className="relative bg-gray-4 border-[1px] border-gray-6 rounded-[26px] shadow-md drop-shadow-0 drop-shadow-y-3 blur-6">
+              <div className="flex items-center justify-center p-4 md:p-5 ">
+                <h3 className="text-center text-xl font-semibold dark:text-white text-small text-white">
+                  Update Billing Details
+                </h3>
+                <button
+                  type="button"
+                  className="absolute right-0 mr-[20px] text-white bg-gray-8 hover:bg-gray-200 hover:text-gray-900 rounded-[55px] shadow-md drop-shadow-0 drop-shadow-y-3 blur-6 text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                  onClick={() => setIsShowUpdateBillingDetails(false)}
+                >
+                  <Image src={closeIcon} width={20} height={20} alt="close" />
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <div className="mx-[50px] my-[10px] flex flex-col justify-center">
+                <div className="w-full bg-gray-10 p-[20px] rounded-md">
+                  <label className="text-[0.93rem] text-[#30313d]">
+                    Card Number
+                  </label>
+                  <div className="bg-white p-[0.75rem] rounded-[5px]">
+                    <CardNumberElement options={CARD_ELEMENT_OPTIONS} />
+                  </div>
+                  <div className="h-[10px]"></div>
+                  <label className="text-[0.93rem] text-[#30313d]">CVC</label>
+                  <div className="bg-white p-[0.75rem] rounded-[5px]">
+                    <CardCvcElement options={CARD_ELEMENT_OPTIONS} />
+                  </div>
+                  <div className="h-[10px]"></div>
+                  <label className="text-[0.93rem] text-[#30313d]">
+                    Expire Date
+                  </label>
+                  <div className="bg-white p-[0.75rem] rounded-[5px]">
+                    <CardExpiryElement options={CARD_ELEMENT_OPTIONS} />
+                  </div>
+                  <div className="h-[10px]"></div>
+                  <AddressElement
+                    options={{
+                      mode: "billing",
+                      autocomplete: {
+                        mode: "automatic",
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-center items-center p-4 md:p-5">
+                <button
+                  disabled={isLoading}
+                  type="button"
+                  className="flex justify-center items-center rounded-[24px] text-white bg-gray-5 px-[30px] py-[12px] shadow-md drop-shadow-0 drop-shadow-y-3 w-[150px] blur-6 mx-[20px]"
+                  onClick={() => {
+                    setIsShowUpdateBillingDetails(false);
+                  }}
+                >
+                  <p>Cancel</p>
+                </button>
+                <button
+                  type="button"
+                  className="flex justify-center items-center rounded-[24px] text-white bg-gray-5 px-[30px] py-[12px] shadow-md drop-shadow-0 drop-shadow-y-3 w-[150px] blur-6 mx-[20px]"
+                  onClick={updateBillingDetails}
+                >
+                  {isLoading && (
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 mr-[10px]"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                  )}
+                  <p>Update</p>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <ToastContainer />
     </div>
   );
 };
 
-export default EditSubscriptionPage;
+const EditSubscriptionPageWrapper = () => {
+  const options = {
+    appearance: {},
+  };
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <EditSubscriptionPage />
+    </Elements>
+  );
+};
+
+export default EditSubscriptionPageWrapper;
